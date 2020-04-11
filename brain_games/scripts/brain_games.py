7fc0192bceb2075b22e7e21c7798b5ed328614cd
Roman-Sergeichuk <sergeichuk.roman@mail.ru>
2020-04-12 00:27:58 +0300

    username request added

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,13 @@
-def main():
+from brain_games.cli import welcome_user
+
+
+def greet():
 	print('Welcome to the Brain Games!')
 
+
+def main():
+	greet()
+	welcome_user()
 
 if __name__ == '__main__':
 	main()
