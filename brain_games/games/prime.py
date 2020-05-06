4988e39916f51269bac45d90990ff656d4870f48
Sergeichuk Roman <sergeichuk.roman@mail.ru>
2020-05-07 00:07:16 +0300

    Optimized prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,4 +1,5 @@
 import random
+from even import is_even
 
 
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
@@ -10,14 +11,12 @@ def is_divided_by(val, divider):
 
 def is_prime(val):
     """this predicate function to check the prime number"""
-    if val < 2:
+    if val < 2 or (val > 2 and is_even(val)):
         return False
     elif val == 2:
         return True
-    elif val > 2 and is_divided_by(val, 2) == 0:
-        return False
     for i in range(3, val, 2):
-        if is_divided_by(val, 3) == 0:
+        if is_divided_by(val, i) == 0:
             return False
     return True
 
