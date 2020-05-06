266e7070267a2cab4ae34e641ed5c7a4156919e2
Sergeichuk Roman <sergeichuk.roman@mail.ru>
2020-05-06 23:35:12 +0300

    Optimized prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -10,15 +10,16 @@ def is_divided_by(val, divider):
 
 def is_prime(val):
     """this predicate function to check the prime number"""
-    if val < 2 or (val > 2 and is_divided_by(val, 2) == 0):
-        result = False
+    if val < 2:
+        return False
     elif val == 2:
-        result = True
+        return True
+    elif (val > 2 and is_divided_by(val, 2) == 0):
+        return False
     for i in range(3, val, 2):
         if is_divided_by(val, 3) == 0:
-            result = False
-    result = True
-    return result
+            return False
+    return True
 
 
 def get_game():
